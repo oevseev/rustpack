9aa9d487e8c6d0af539cdea725a567976ffb46c3
Oleg Evseev <git@x3n.me>
2022-09-12 02:27:09 +0400

    Also process target source paths from manifest

--- src/bundle.rs
+++ src/bundle.rs
@@ -1,11 +1,11 @@
 use camino::Utf8Path;
 
-use crate::manifest::get_crate_paths;
+use crate::manifest::process_manifest;
 
 pub fn bundle_bin(manifest_dir: &Utf8Path, out_dir: &Utf8Path, bin: Option<&str>) {
-    println!("{:#?}", get_crate_paths(manifest_dir))
+    println!("{:#?}", process_manifest(manifest_dir))
 }
 
 pub fn bundle_all(manifest_dir: &Utf8Path, out_dir: &Utf8Path) {
-    println!("{:#?}", get_crate_paths(manifest_dir))
+    println!("{:#?}", process_manifest(manifest_dir))
 }
--- src/manifest.rs
+++ src/manifest.rs
@@ -1,12 +1,18 @@
 use std::collections::HashMap;
 
 use camino::{Utf8PathBuf, Utf8Path};
-use cargo_metadata::{MetadataCommand, Package, Target};
+use cargo_metadata::{MetadataCommand, Package, Target, Metadata};
 
 #[derive(Debug)]
 pub(crate) struct CratePaths {
     manifest_dir: Utf8PathBuf,
     src_path: Utf8PathBuf,
+}
+
+#[derive(Debug)]
+pub(crate) struct Paths {
+    crate_paths: HashMap<String, CratePaths>,
+    target_paths: HashMap<String, Utf8PathBuf>,
 }
 
 fn get_pkg_manifest_dir(pkg: &Package) -> Utf8PathBuf {
@@ -22,25 +28,40 @@ fn get_lib_crate_name(target: &Target) -> String {
     target.name.replace("-", "_")
 }
 
-fn get_lib_crate_relative_src_path(pkg: &Package, target: &Target) -> Utf8PathBuf {
+fn get_relative_src_path(pkg: &Package, target: &Target) -> Utf8PathBuf {
     target.src_path
         .strip_prefix(pkg.manifest_path.parent().unwrap())
         .unwrap()
         .to_owned()
 }
 
-pub(crate) fn get_crate_paths(manifest_dir: &Utf8Path) -> HashMap<String, CratePaths> {
-    let metadata = MetadataCommand::new()
-        .manifest_path(manifest_dir.join("Cargo.toml"))
-        .exec()
-        .unwrap();
-        
+fn get_crate_paths(metadata: &Metadata) -> HashMap<String, CratePaths> {    
     metadata.packages.iter()
         // Use filter_map because are interested only in library crates
         .filter_map(|pkg| get_lib_crate_target(pkg)
             .map(|target| (get_lib_crate_name(target), CratePaths {
                 manifest_dir: get_pkg_manifest_dir(pkg),
-                src_path: get_lib_crate_relative_src_path(pkg, target),
+                src_path: get_relative_src_path(pkg, target),
             })))
         .collect()
+}
+
+fn get_target_paths(metadata: &Metadata) -> HashMap<String, Utf8PathBuf> {
+    let root_pkg = metadata.root_package().expect("failed to determine root package");
+
+    root_pkg.targets.iter()
+        .map(|target| (target.name.clone(), get_relative_src_path(root_pkg, target)))
+        .collect()
+}
+
+pub(crate) fn process_manifest(manifest_dir: &Utf8Path) -> Paths {
+    let metadata = MetadataCommand::new()
+        .manifest_path(manifest_dir.join("Cargo.toml"))
+        .exec()
+        .unwrap();
+
+    Paths {
+        crate_paths: get_crate_paths(&metadata),
+        target_paths: get_target_paths(&metadata),
+    }
 }
